901536bfd59184d0cf5c78b635ea658946ed625c
KingJunJie <Wjj11231123>
2020-07-11 14:49:53 +0800

    leetcode添加新方法，简单粗暴

--- absttstu/src/main/java/com/leetcode/algorithm/simple/Solution.java
+++ absttstu/src/main/java/com/leetcode/algorithm/simple/Solution.java
@@ -1,5 +1,8 @@
 package com.leetcode.algorithm.simple;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -17,10 +20,10 @@ public class Solution {
     public static void main(String[] args) {
         int[] nums = {1, 2, 6, 8, 10};
         int target = 10;
-        new Solution().twoSum(nums, target);
+        new Solution().twoSum02(nums, target);
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum01(int[] nums, int target) {
         int[] result = new int[2];
         System.out.println("返回数组长度" + result.length);
         for (int i = 0; i < nums.length; i++) {
@@ -38,4 +41,16 @@ public class Solution {
         }
         return result;
     }
+
+    public int[] twoSum02(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length ; i++) {
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i,map.get(nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
 }
